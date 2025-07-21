5119f7d8821fe832a157a57b274d0c493b511611
chris romeo <chrisdemonxxx@gmail.com>
2025-07-21 07:18:43 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
-// websocket-server/server.js
 const http = require('http');
 const WebSocket = require('ws');
-// const { MongoClient } = require('mongodb'); // Uncomment if using DB
+const { MongoClient } = require('mongodb');
 
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
-// Uncomment below if using a database
-const MONGO_URI=mongodb+srv://chrisdemonxxx:QrsdvAf7K3ZVcwRG@cluster0.lsyii5u.mongodb.net/loopjs?retryWrites=true&w=majority&appName=Cluster0
+let db = null;
+
+(async () => {
+  const client = new MongoClient(MONGO_URI);
+  await client.connect();
+  db = client.db('loopjs');
+  console.log('Connected to MongoDB');
+})();
 
 wss.on('connection', (ws) => {
-    console.log("Client connected.");
+  console.log("Client connected.");
 
-    ws.on('message', (data) => {
-        console.log("Received:", data.toString());
+  ws.on('message', async (data) => {
+    const message = data.toString();
+    console.log("Received:", message);
 
-        // Echo something back to your Qt app
-        ws.send("FROM_SERVERsep-x8jmjgfmr9messageboxsep-x8jmjgfmr9Hello,This is from Node,info");
+    // Save to DB
+    if (db) {
+      await db.collection('messages').insertOne({
+        text: message,
+        at: new Date()
+      });
+    }
 
-        // Example DB save (if needed):
-        // db.collection('messages').insertOne({ content: data.toString(), at: new Date() });
-    });
+    ws.send("FROM_SERVERsep-x8jmjgfmr9messageboxsep-x8jmjgfmr9Hello,This is from Node,info");
+  });
 
-    ws.on('close', () => {
-        console.log("Client disconnected.");
-    });
+  ws.on('close', () => {
+    console.log("Client disconnected.");
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`WebSocket server running on port ${PORT}`);
+  console.log(`WebSocket server running on port ${PORT}`);
 });
-
-/*
-(async () => {
-    const client = new MongoClient(MONGO_URI);
-    await client.connect();
-    const db = client.db('mydb');
-    console.log('Connected to MongoDB');
-})();
-*/
